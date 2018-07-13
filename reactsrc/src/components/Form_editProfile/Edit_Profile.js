52ab798a2d4657c8e82e4fcae2325248bd34040c
nicko marselino <nickomarsellino@gmail.com>
2018-07-13 22:27:10 +0700

    Update Profile Page

--- reactsrc/src/components/Form_editProfile/Edit_Profile.js
+++ reactsrc/src/components/Form_editProfile/Edit_Profile.js
@@ -12,7 +12,6 @@ class Edit_Profile extends Component {
         super(props);
 
         this.state = {
-            users: [],
             username: "",
             email: "",
             password: "",
@@ -30,7 +29,6 @@ class Edit_Profile extends Component {
         const name = target.name;
 
         this.setState({ [name]: target.value });
-
     }
 
     handleSubmit(e){
@@ -70,6 +68,7 @@ class Edit_Profile extends Component {
                                       <Form onSubmit={this.handleSubmit}>
                                           <Form.Input required type="text" fluid label='Username'
                                                       placeholder={this.state.username}
+                                                      value={this.state.username}
                                                       className={this.state.formStatus}
                                                       onChange={this.handleInputChange}
                                                       name="username"
@@ -77,6 +76,7 @@ class Edit_Profile extends Component {
 
                                           <Form.Input required type="email" fluid label='Email'
                                                       placeholder={this.state.email}
+                                                      value={this.state.email}
                                                       className={this.state.formStatus}
                                                       onChange={this.handleInputChange}
                                                       name="email"
@@ -91,6 +91,7 @@ class Edit_Profile extends Component {
 
                                           <Form.Input required type="number" fluid label='Phone Number'
                                                       placeholder={this.state.phone}
+                                                      value={this.state.phone}
                                                       className={this.state.formStatus}
                                                       onChange={this.handleInputChange}
                                                       name="phone"
