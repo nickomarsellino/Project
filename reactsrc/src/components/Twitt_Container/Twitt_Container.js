7c311861d748cf044bf19a85ab1d188c9978df5d
Cehaaaaa <Christian.irwandi@gmail.com>
2018-07-19 10:58:45 +0700

    Revisiii get data

--- reactsrc/src/components/Twitt_Container/Twitt_Container.js
+++ reactsrc/src/components/Twitt_Container/Twitt_Container.js
@@ -5,47 +5,48 @@ import profile from '../../daniel.jpg';
 import axios from 'axios';
 
 class Twitt_Container extends Component {
+
   constructor(){
-        super();
-        this.state = {
-            tweetData : []
-        }
-        this.getData = this.getData.bind(this)
-    }
+          super();
+          this.state = {
+              tweetData : []
+          }
+          this.getData = this.getData.bind(this)
+      }
 
-    componentWillMount() {
-        this.getData();
-    }
+      componentWillMount() {
+          this.getData();
+      }
 
-    getData() {
-      axios.get('/api/users/tweets')
-          .then(res => {
-              this.setState({
-                  tweetData : res.data
-              });
-              console.log("respon GET req ", this.state.tweetData);
-          });
-    }
+      getData() {
+        axios.get('/api/users/tweets')
+            .then(res => {
+                this.setState({
+                    tweetData : res.data
+                });
+                console.log("respon GET req ", this.state.tweetData);
+            });
+      }
 
     render() {
         return (
             <div>
-               
+              {this.state.tweetData.map(tweet =>
                 <Card style={{marginTop: "3px"}}>
                     <CardBody style={{padding: "10px"}}>
                         <Feed>
                             <Feed.Event>
-                                    <Feed.Label image={profile} style={{width: "10%",  padding: "5px 0"}} />
-                                    <Feed.Content>
-                                        <Feed.Summary content={ } />
-                                        <Feed.Extra text content={ } /><br />
-                                        <Feed.Date content={ } />
-                                    </Feed.Content>
+                                <Feed.Label image={profile} style={{width: "10%",  padding: "5px 0"}}/>
+                                <Feed.Content>
+                                    <Feed.Summary content={tweet._id} />
+                                    <Feed.Extra text content={tweet.tweetText} /> <br />
+                                    <Feed.Date content={tweet.timestamp} />
+                                </Feed.Content>
                             </Feed.Event>
                         </Feed>
                     </CardBody>
                 </Card>
-               
+              )}
             </div>
         );
     }
