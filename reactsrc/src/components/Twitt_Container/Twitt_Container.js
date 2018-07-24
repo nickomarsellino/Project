8a094b9fda2b4ef2d2f389e30528fcff8b38c93a
nicko marselino <nickomarsellino@gmail.com>
2018-07-24 16:40:21 +0700

    Penyempurnaan Tampilan

--- reactsrc/src/components/Twitt_Container/Twitt_Container.js
+++ reactsrc/src/components/Twitt_Container/Twitt_Container.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Card, CardBody} from "mdbreact"
-import { Feed,  Icon } from 'semantic-ui-react';
+import {Feed, Icon} from 'semantic-ui-react';
 import profile from '../../daniel.jpg';
 import axios from 'axios';
 import './Twiit_Container.css';
@@ -12,21 +12,22 @@ import Modal_Delete from '../Modal_Delete/Modal_Delete';
 const Timestamp = require('react-timestamp');
 
 
-
 class Twitt_Container extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            tweetData : [],
-            tweet : [],
-            modalTwiit: false,
+            tweetData: [],
+            tweet: [],
+            modalTweet: false,
             modalDelete: false
         };
 
         this.getData = this.getData.bind(this);
-        this.openModal = this.openModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
+        this.openModalTweet = this.openModalTweet.bind(this);
+        this.openModalDelete = this.openModalDelete.bind(this);
+        this.closeModalTweet = this.closeModalTweet.bind(this);
+        this.closeModalDelete = this.closeModalDelete.bind(this);
     }
 
     componentWillMount() {
@@ -37,38 +38,55 @@ class Twitt_Container extends Component {
         axios.get('/api/users/tweets')
             .then(res => {
                 this.setState({
-                    tweetData : res.data
+                    tweetData: res.data
                 });
                 console.log("tweetData ", this.state.tweetData);
             });
     }
 
-    openModal(tweetId){
-        axios.get('http://localhost:3000/api/users/tweet/'+tweetId)
+    openModalTweet(tweetId) {
+        axios.get('http://localhost:3000/api/users/tweet/' + tweetId)
             .then(res => {
                 this.setState({
-                    tweet : res.data,
-                    modalTwiit: true
+                    tweet: res.data,
+                    modalTweet: true
                 });
             });
     }
 
-    closeModal(isOpen) {
-        if(isOpen){
+    openModalDelete() {
+        this.setState({
+            modalDelete: true
+        });
+    }
+
+    closeModalTweet(isOpen) {
+        if (isOpen) {
             {
                 this.setState({
-                    modalTwiit: false
+                    modalTweet: false
                 })
             }
         }
     }
 
-    buttonDelete(userId){
-        if(userId == this.props.userId){
+    closeModalDelete(isOpen) {
+        if (isOpen) {
+            {
+                this.setState({
+                    modalDelete: false
+                })
+            }
+        }
+    }
+
+    buttonDelete(userId) {
+        if (userId == this.props.userId) {
             return (
                 <Icon className="Tweet-Content"
                       size='large' name='trash'
                       id="recycleIcon"
+                      onClick={this.openModalDelete}
                 />
             );
         }
@@ -78,21 +96,20 @@ class Twitt_Container extends Component {
         return (
             <div>
                 {this.state.tweetData.map(tweet =>
-                    <Card className="Tweet_Container"
-                          onClick={() => this.openModal(tweet._id)}>
+                    <Card className="Tweet_Container">
                         <CardBody className="Tweet">
                             <Feed>
-                                <Feed.Event>
-                                    <Feed.Label image={profile} style={{width: "10%",  padding: "5px 0"}}/>
+                                <Feed.Event onClick={() => this.openModalTweet(tweet._id)}>
+                                    <Feed.Label image={profile} style={{width: "10%", padding: "5px 0"}}/>
                                     <Feed.Content>
-                                        <div className="Tweet-Content" >
-                                            <Feed.Summary content={tweet.username} />
+                                        <div className="Tweet-Content">
+                                            <Feed.Summary content={tweet.username}/>
                                         </div>
 
                                         {this.buttonDelete(tweet.userId)}
 
-                                        <Feed.Extra text content={tweet.tweetText} /> <br />
-                                        <Feed.Date content={<Timestamp time={tweet.timestamp} precision={1} />} />
+                                        <Feed.Extra text content={tweet.tweetText}/> <br/>
+                                        <Feed.Date content={<Timestamp time={tweet.timestamp} precision={1}/>}/>
                                     </Feed.Content>
                                 </Feed.Event>
                             </Feed>
@@ -101,15 +118,15 @@ class Twitt_Container extends Component {
                 )}
 
                 <Modal_Twitt
-                isOpen = {this.state.modalTwiit}
-                tweet = {this.state.tweet}
-                isClose = {this.closeModal}
+                    isOpen={this.state.modalTweet}
+                    tweet={this.state.tweet}
+                    isClose={this.closeModalTweet}
                 />
 
                 <Modal_Delete
-                    isOpen = {this.state.modalOpen}
-                    tweet = {this.state.tweet}
-                    isClose = {this.closeModal}
+                    isOpen={this.state.modalDelete}
+                    tweet={this.state.tweet}
+                    isClose={this.closeModalDelete}
                 />
 
             </div>
