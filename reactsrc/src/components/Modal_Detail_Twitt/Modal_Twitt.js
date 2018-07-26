17469b05d448cf044f35d367ca3b93330ff48c5f
Cehaaaaa <Christian.irwandi@gmail.com>
2018-07-26 10:20:07 +0700

    backup ch

--- reactsrc/src/components/Modal_Detail_Twitt/Modal_Twitt.js
+++ reactsrc/src/components/Modal_Detail_Twitt/Modal_Twitt.js
@@ -1,50 +1,42 @@
 import React, {Component} from "react";
-import { Container, Button, Modal, ModalBody, ModalHeader} from 'mdbreact';
+import {Container, Button, Modal, ModalBody, ModalHeader} from 'mdbreact';
 import axios from "axios/index";
 const Timestamp = require('react-timestamp');
 
-
 class Modal_Twitt extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            tweet: []
-        };
-        this.openModal = this.openModal.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      tweet: []
+    };
+    this.openModal = this.openModal.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.tweet !== this.props.tweet) {
+      this.setState({tweet: nextProps.tweet});
     }
+  }
 
+  openModal() {
+    //Akses Fungsi yang di Twitt_Container.js
+    this.props.isClose(this.props.isOpen);
+  }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.tweet !== this.props.tweet) {
-            this.setState({
-                tweet : nextProps.tweet
-            });
-        }
-    }
-
-    openModal(){
-        //Akses Fungsi yang di Twitt_Container.js
-        this.props.isClose(this.props.isOpen);
-    }
-
-
-    render() {
-        return (
-            <Container>
-                <Modal isOpen={this.props.isOpen} toggle={this.openModal}>
-                    <ModalHeader toggle={this.openModal}>{this.state.tweet.username}</ModalHeader>
-                    <ModalBody>
-                        {this.state.tweet.tweetText}
-                    </ModalBody>
-                    <ModalBody>
-                        <Timestamp time={this.state.tweet.timestamp} format='full' includeDay />
-                    </ModalBody>
-                </Modal>
-            </Container>
-        );
-    }
+  render() {
+    return (<Container>
+      <Modal isOpen={this.props.isOpen} toggle={this.openModal}>
+        <ModalHeader toggle={this.openModal}>{this.state.tweet.username}</ModalHeader>
+        <ModalBody>
+          {this.state.tweet.tweetText}
+        </ModalBody>
+        <ModalBody>
+          <Timestamp time={this.state.tweet.timestamp} format='full' includeDay="includeDay"/>
+        </ModalBody>
+      </Modal>
+    </Container>);
+  }
 }
-
 
 export default Modal_Twitt;
