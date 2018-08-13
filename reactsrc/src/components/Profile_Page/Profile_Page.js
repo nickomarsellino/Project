436be1efbf98c66d44d65b8cbdd5bd58b6241fab
nicko marselino <nickomarsellino@gmail.com>
2018-08-13 15:55:01 +0700

    Pembenahan Code & Penambahan di Profile Page

--- reactsrc/src/components/Profile_Page/Profile_Page.js
+++ reactsrc/src/components/Profile_Page/Profile_Page.js
@@ -5,6 +5,7 @@ import './Profile_Page.css';
 import TwittContainer from "../Twitt_Container/Twitt_Container";
 import {Icon} from 'semantic-ui-react';
 import FadeIn from 'react-fade-in';
+import ReactDOM from "react-dom";
 
 const Timestamp = require('react-timestamp');
 
@@ -17,10 +18,14 @@ class Edit_Profile extends Component {
             timestamp: '',
             email: '',
             phone: '',
-            tweetCount: ''
+            tweetCount: '',
+            tweetItem: true,
+            followingItem: false,
+            followerItem: false
         };
 
         this.getTweetCounter = this.getTweetCounter.bind(this);
+        this.handleItemClicked = this.handleItemClicked.bind(this);
     }
 
     componentWillMount() {
@@ -43,6 +48,30 @@ class Edit_Profile extends Component {
         });
     }
 
+    handleItemClicked(item){
+
+        console.log(item);
+
+        if(item === "Follower"){
+            //Render Validation box message
+            ReactDOM.render(<FadeIn>
+                <p>Follower</p>
+            </FadeIn>, document.getElementById('profileInfo'));
+        }
+        else if(item === "Following"){
+            //Render Validation box message
+            ReactDOM.render(<FadeIn>
+                <p>Following</p>
+            </FadeIn>, document.getElementById('profileInfo'));
+        }
+        else if(item === "Tweets"){
+            //Render Validation box message
+            ReactDOM.render(<FadeIn><TwittContainer TweetUserId={this.props.userId}
+                                               userId={this.props.userId}
+                                               tweetCounter={this.getTweetCounter}
+            /></FadeIn>, document.getElementById('profileInfo'));
+        }
+    }
 
     render() {
         return (
@@ -69,18 +98,17 @@ class Edit_Profile extends Component {
                     </div>
 
                     <div id="navDetail" className="ui three item menu">
-                        <a className="item">Tweets <br/><br/>{this.state.tweetCount}</a>
-                        <a className="item">Following <br/><br/>15</a>
-                        <a className="item">Followers <br/><br/>15</a>
+                        <a className="item" onClick={() => this.handleItemClicked("Tweets")}> Tweets <br/><br/>{this.state.tweetCount}</a>
+                        <a className="item" onClick={() => this.handleItemClicked("Following")}>Following <br/><br/>15</a>
+                        <a className="item" onClick={() => this.handleItemClicked("Follower")}>Followers <br/><br/>15</a>
                     </div>
 
-                    <div className="userTweet">
+                    <div className="userProfile" id="profileInfo">
                         <TwittContainer TweetUserId={this.props.userId}
                                         userId={this.props.userId}
                                         tweetCounter={this.getTweetCounter}
                         />
                     </div>
-
                 </div>
             </FadeIn>
 
