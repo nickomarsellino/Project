61cf877bf6215d0b185bfd78c3b34f8786a4ce38
nicko marselino <nickomarsellino@gmail.com>
2018-10-24 16:07:15 +0700

    Pembenahan fitur following & followed

--- reactsrc/src/components/UserCardComponent/UserCardComponent.js
+++ reactsrc/src/components/UserCardComponent/UserCardComponent.js
@@ -24,14 +24,14 @@ class UserCardComponent extends Component {
     }
 
     componentDidMount() {
-        if(this.props.located === "inSearchPage"){
+        if (this.props.located === "inSearchPage") {
             this.setState({
                 userData: this.props.userData
             });
             // //Check apakah user tersebut memfollow yang sedang login
             axios.get('/api/users/profile/' + localStorage.getItem("myThings")).then(res => {
                 const user = res.data[0];
-                if(user.following.includes(this.props.userData._id)){
+                if (user.following.includes(this.props.userData._id)) {
                     this.setState({
                         isFollow: true,
                         buttonFollowText: "Followed",
@@ -41,13 +41,12 @@ class UserCardComponent extends Component {
             });
         }
         else {
-            console.log(this.props.userLoginFollowingData);
             this.getSpecificFollowingUser();
         }
     }
 
-    getSpecificFollowingUser(){
-        axios.get('/api/users/profile/'+this.props.userData)
+    getSpecificFollowingUser() {
+        axios.get('/api/users/profile/' + this.props.userData)
             .then(res => {
                 this.setState({
                     userData: res.data[0]
@@ -55,7 +54,7 @@ class UserCardComponent extends Component {
             });
 
         //Check apakah user tersebut memfollow yang sedang login
-        if(this.props.userLoginFollowingData.includes(this.props.userData)){
+        if (this.props.userLoginFollowingData.includes(this.props.userData)) {
             this.setState({
                 isFollow: true,
                 buttonFollowText: "Followed",
@@ -64,76 +63,76 @@ class UserCardComponent extends Component {
         }
     }
 
-   setProfileImage(profilePicture) {
-       let imageUrl = profilePicture;
+    setProfileImage(profilePicture) {
+        let imageUrl = profilePicture;
 
-       if (imageUrl) {
-           return (
-               <img alt=" " id="ProfileImage" src={require(`../../uploads/${imageUrl}`)} className="float-right"/>
-           );
-       }
-       else {
-           return (
-               <img alt=" " src={profile} id="ProfileImage"/>
-           );
-       }
-     }
+        if (imageUrl) {
+            return (
+                <img alt=" " id="ProfileImage" src={require(`../../uploads/${imageUrl}`)} className="float-right"/>
+            );
+        }
+        else {
+            return (
+                <img alt=" " src={profile} id="ProfileImage"/>
+            );
+        }
+    }
 
-   viewUserProfile(username, userId) {
-       if (localStorage.getItem("myThings") === userId) {
-           this.props.history.push({
-               pathname: `/home/myProfile/${username}`.replace(' ', ''),
-           })
-       }
-       else {
-           this.props.history.push({
-               pathname: `/home/profile/${username}`.replace(' ', ''),
-               state: {
-                   userId: userId
-               }
-           })
-       }
-   }
+    viewUserProfile(username, userId) {
+        if (localStorage.getItem("myThings") === userId) {
+            this.props.history.push({
+                pathname: `/home/myProfile/${username}`.replace(' ', ''),
+            })
+        }
+        else {
+            this.props.history.push({
+                pathname: `/home/profile/${username}`.replace(' ', ''),
+                state: {
+                    userId: userId
+                }
+            })
+        }
+    }
 
 
-   mouseEnter() {
-       if (this.state.isFollow) {
-           this.setState({buttonFollowText: "Unfollow"})
-       }
-       else {
-           this.setState({buttonFollowText: "Follow"})
-       }
+    mouseEnter() {
+        if (this.state.isFollow) {
+            this.setState({buttonFollowText: "Unfollow"})
+        }
+        else {
+            this.setState({buttonFollowText: "Follow"})
+        }
 
-   }
+    }
 
-   mouseLeave() {
-       if (this.state.isFollow) {
-           this.setState({buttonFollowText: "Followed"})
-       }
-       else {
-           this.setState({buttonFollowText: "Follow"})
-       }
-   }
+    mouseLeave() {
+        if (this.state.isFollow) {
+            this.setState({buttonFollowText: "Followed"})
+        }
+        else {
+            this.setState({buttonFollowText: "Follow"})
+        }
+    }
 
-    onButtonClicked(userId){
-        this.setState({ isFollow: !this.state.isFollow });
+    onButtonClicked(userId) {
+        this.setState({isFollow: !this.state.isFollow});
 
-        if(this.state.isFollow){
+        if (this.state.isFollow) {
             axios.put('/api/users/unfollow/' + userId).then(res => {
-                this.setState({ butttonFollowCondition: "followButton"})
+                this.setState({butttonFollowCondition: "followButton"})
             });
         }
-        else{
+        else {
             axios.put('/api/users/follow/' + userId).then(res => {
-                this.setState({ butttonFollowCondition: "followedButton"})
+                this.setState({butttonFollowCondition: "followedButton"})
             });
         }
     }
 
 
-    followButton(userId){
+    followButton(username, userId) {
 
-        if(userId !== localStorage.getItem("myThings")){
+        if (userId !== localStorage.getItem("myThings")) {
             return (
                 <div
                     id={this.state.butttonFollowCondition}
@@ -154,37 +153,48 @@ class UserCardComponent extends Component {
         }
         else {
             return (
-                <div id="isUserAccount"/>
+                <div
+                    id={this.state.butttonFollowCondition}
+                    onClick={() => this.viewUserProfile(username , userId)}>
+                    <center>
+                        <Icon
+                            size='large'
+                            name='user circle'
+                            id='iconFollow'
+                        />
+                        {' '} My Profile
+                    </center>
+                </div>
             );
         }
     }
 
 
-   render() {
-       return (
-           <div className="col-lg-3 col-lg-offset-4 user-Container" key={this.state.userData._id}>
-               <Card>
-                   <center>
-                       <Image style={{margin: "20px"}}
-                              onClick={() => this.viewUserProfile(this.state.userData.username, this.state.userData._id)}>
-                           {this.setProfileImage(this.state.userData.profilePicture)}
-                       </Image>
-                   </center>
-                   <Card.Content>
-                       <center>
-                           <Card.Header className="profileName"
-                                        onClick={() => this.viewUserProfile(this.state.userData.username, this.state.userData._id)}>
-                               {this.state.userData.username}
-                           </Card.Header>
+    render() {
+        return (
+            <div className="col-lg-3 col-lg-offset-4 user-Container" key={this.state.userData._id}>
+                <Card>
+                    <center>
+                        <Image style={{margin: "20px"}}
+                               onClick={() => this.viewUserProfile(this.state.userData.username, this.state.userData._id)}>
+                            {this.setProfileImage(this.state.userData.profilePicture)}
+                        </Image>
+                    </center>
+                    <Card.Content>
+                        <center>
+                            <Card.Header className="profileName"
+                                         onClick={() => this.viewUserProfile(this.state.userData.username, this.state.userData._id)}>
+                                {this.state.userData.username}
+                            </Card.Header>
 
-                           {this.followButton(this.state.userData._id)}
+                            {this.followButton(this.state.userData.username, this.state.userData._id)}
 
-                       </center>
-                   </Card.Content>
-               </Card>
-           </div>
-       );
-   }
+                        </center>
+                    </Card.Content>
+                </Card>
+            </div>
+        );
+    }
 }
 
 export default UserCardComponent;
