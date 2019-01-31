65e55ccdc287a8282928b89dea784208195b71df
nicko marselino <nickomarsellino@gmail.com>
2019-01-31 13:33:44 +0700

    Pembuatan Notif

--- reactsrc/src/components/Notification/Notification_Container/Notification_Container.js
+++ reactsrc/src/components/Notification/Notification_Container/Notification_Container.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import FadeIn from 'react-fade-in';
 import {Container} from "mdbreact";
+import './Notification_Container.css'
 import axios from 'axios';
 
 //Load another component
@@ -18,10 +19,9 @@ class Notification_Container extends Component {
     render() {
         return (
             <FadeIn>
-                <Container className="col-lg-8 col-lg-offset-4" style={{marginBottom: "5%", marginTop: "5%"}}>
-                    <div>
-                        HAhahahaha
-                    </div>
+                <Container className="col-lg-6 col-lg-offset-6" style={{marginBottom: "5%", marginTop: "3%"}}>
+                        <h2 id="titlePage">Your Notification</h2>
+                        <hr/>
                 </Container>
             </FadeIn>
         )
